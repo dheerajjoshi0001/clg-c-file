fc726e1173a3f4beeeff74f135c52f1cec594026
dheeraj <dhirajjoshi.987@gmail.com>
2024-10-05 20:46:02 +0530

    to print name and adress

---
+++ prac 1/1.c
@@ -0,0 +1,11 @@
+/* Print your name and Address*/
+
+ #include <stdio.h>
+
+int main() {
+    // Print name and address
+    printf("Name:dheeraj\n");
+    printf("Address: girital\n");
+    
+    return 0;
+}
